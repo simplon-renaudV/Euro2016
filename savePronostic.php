470a9a54f4442d3c93cee123ad60cb9a5749f70c
simplon-renaudV <renaud.vivancos@free.fr>
2016-06-21 22:46:20 +0200

    enregistrement pronostic en bdd

--- savePronostic.php
+++ savePronostic.php
@@ -1,9 +1,18 @@
 <?php
-
+	include('PDO.php');
 	include_once('Class/Competition.class.php');
 	include_once('Class/Groups.class.php');
 
 	$euro2016 = new Competition('Json/competition.json');
+
+	$pronostic = $bdd->prepare("INSERT INTO Pronostics (groupe, pays1, pays2, pr1, pr2) VALUES (:groupe, :pays1, :pays2, :pr1, :pr2)");
+	$pronostic->execute(array(
+		'groupe'=>$_GET['grp'],
+		'pays1'=>$_GET['p1'],
+		'pays2'=>$_GET['p2'],
+		'pr1'=>$_POST['prScoreEq1'],
+		'pr2'=>$_POST['prScoreEq2']
+	));
 
 	echo 'Groupe '.$_GET['grp'].'<br/>';
 	echo 'Match : '.$_GET['p1'].' - '.$_GET['p2'].'<br/>';
